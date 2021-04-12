fab27edfd8f851016551e9d4d3007e53727be73d
PiotrGrobelak <grobelak.piotr@gmail.com>
2021-04-12 19:23:02 +0200

    Add JS logic

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-console.log('Hello IOS PWA')
+
+const nodeElements = {
+    mainBtn: document.querySelector(".main__button"),
+    iosPwaPromptOverlay: document.querySelector('.ios-pwa-prompt__overlay--js'),
+    iosPwaPromptContainer: document.querySelector('.ios-pwa-prompt__container--js'),
+    iosPwaPromptBtn: document.querySelector('.ios-pwa-header__button--js')
+}
+
+
+function toggleIosPwaPrompt() {
+    const { iosPwaPromptOverlay, iosPwaPromptContainer} = nodeElements;
+    iosPwaPromptOverlay.classList.toggle("ios-pwa-prompt__overlay--hidden")
+    iosPwaPromptContainer.classList.toggle("ios-pwa-prompt__container--hidden")
+    iosPwaPromptOverlay.classList.toggle("ios-pwa-prompt__overlay--visible")
+    iosPwaPromptContainer.classList.toggle("ios-pwa-prompt__container--visible")
+}
+
+function onTogglePwaPrompt(nodeElement) {
+    nodeElement.addEventListener('click', function (e) {
+        e.stopPropagation();
+        toggleIosPwaPrompt();
+    })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    onTogglePwaPrompt(nodeElements.mainBtn)
+    onTogglePwaPrompt(nodeElements.iosPwaPromptOverlay)
+    onTogglePwaPrompt(nodeElements.iosPwaPromptBtn)
+
+    setTimeout(()=> {
+        toggleIosPwaPrompt()
+    }, 1000)
+});
+
+
+
+
+
+
